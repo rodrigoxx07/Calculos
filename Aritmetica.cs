d0227d289e93aa669a6b7bb622b9d5e0c283779f
Rute <92311734+atomant78@users.noreply.github.com>
2023-10-16 17:09:16 +0100

    Criar a Classe Temperatura e mover método

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -15,12 +15,7 @@ namespace Calculos
         /// <summary>
         /// Tipo de Conversão de Teperatura
         /// </summary>
-        public static enum ConversaoTemperatura
-        {
-            Nulo = 0,
-            CelsiusFahrenheit,
-            FahrenheitCelsius
-        }
+      
         public static int Somar(int x, int y)
         {
             return x + y;
@@ -34,19 +29,6 @@ namespace Calculos
         public static int Subtrair(int x, int y)
         {
             return x - y;
-        }
-
-        public static double ConverterTemperatura(ConversaoTemperatura conversao, double temperatura)
-        {
-            if (conversao == ConversaoTemperatura.CelsiusFahrenheit)
-            {
-                return (temperatura * 1.800 + 32);
-            }
-            else if (conversao ==ConversaoTemperatura.FahrenheitCelsius)
-            {
-                return((temperatura - 32) / 1.800);
-            }
-            return -1;
         }
 
     }
---
+++ Temperatura.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Calculos.Aritmetica;
+
+namespace Calculos
+{
+    /// <summary>
+    /// Classe que implementa operações de conversão
+    /// 
+    /// </summary>
+    internal static class Temperatura
+    {
+        public static enum ConversaoTemperatura
+        {
+            Nulo = 0,
+            CelsiusFahrenheit,
+            FahrenheitCelsius
+        }
+
+        /// <summary>
+        /// Conversã de temperaturas
+        /// </summary>
+        /// <param name="conversao"> A conversão a efetuar</param>
+        /// <param name="temperatura">A temperatura a converter</param>
+        /// <returns>Resultado da conversão</returns>
+        public static double ConverterTemperatura(ConversaoTemperatura conversao, double temperatura)
+        {
+            if (conversao == ConversaoTemperatura.CelsiusFahrenheit)
+            {
+                return (temperatura * 1.800 + 32);
+            }
+            else if (conversao == ConversaoTemperatura.FahrenheitCelsius)
+            {
+                return ((temperatura - 32) / 1.800);
+            }
+            return -1;
+        }
+
+    }
+}
