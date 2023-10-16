1924767fbb69ecada9d7ad5afe5f6f7591c0b66c
Rute <92311734+atomant78@users.noreply.github.com>
2023-10-16 16:52:50 +0100

    Adicionar comentários aos métods somar e subtair

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -8,12 +8,23 @@ namespace Calculos
 {
     /// <summary>
     /// Implementa operações aritmeticas
+    /// Operação soma e subtração de dois nrs
     /// </summary>
     internal class Aritmetica
     {
         public int Somar(int x, int y)
         {
             return x + y;
+        }
+        /// <summary>
+        /// operação de subtração d edois numeros retorna o seu resultado
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public int Subtrair(int x, int y)
+        {
+            return x - y;
         }
     }
 }
